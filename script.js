17ad1634bf126acdad01bd2665c1303c4381043e
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2025-12-07 16:17:51 +0530

    Adding the structure of Portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+project = document.getElementById('project')
+project.addEventListener(
+    'click',()=>alert("Projects clicked")
+)
+skill = document.getElementById('skill')
+skill.addEventListener(
+    'click',()=>alert("Skills clicked")
+)
+connect = document.getElementById('connect')
+connect.addEventListener(
+    'click',()=>alert("Connect clicked")
+)
